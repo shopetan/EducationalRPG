f44d05c71bff9d9ef151a5b5c3262a2af1c114b5
minami <s1311389@coins.tsukuba.ac.jp>
2015-09-16 00:53:43 +0900

    completeマークを表示

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ enchant();
 //DBから受け取るユーザーの進捗情報
 var state = [0,4,1,2,3]; //国数理社英
 
-var dungeonX = [80,310,570,20,480];
-var dungeonY = [140,300,225,400,500];
+var dungeonOrigin = [[80,140],[310,300],[570,225],[20,400],[480,500]];
+var islandOrigin = [[0,5],[544,5],[0,344],[544,344],[272,172]];
+var islandImage = ['img/island_j.png', 'img/island_m.png', 'img/island_sc.png', 'img/island_so.png', 'img/island_e.png'];
 
 var subject = {
  japanese: 0,
@@ -18,34 +19,19 @@ var number_of_dungeon = 5;
 
 window.onload = function() {
 	var core = new Core(800, 600);
-	core.preload('img/worldMapBg.jpg','img/island_e.png','img/island_j.png','img/island_m.png','img/island_sc.png','img/island_so.png','img/Japanese.png','img/dungeon.png','img/dungeonMap.jpg','img/board_e.png','img/arrow.png');
+	core.preload('img/worldMapBg.jpg','img/island_e.png','img/island_j.png','img/island_m.png','img/island_sc.png','img/island_so.png','img/Japanese.png','img/dungeon.png','img/dungeonMap.jpg','img/board_e.png','img/arrow.png','img/complete.png');
 
 	var Island = Class.create(Sprite, {
 		initialize: function(x, y, subject) {
 			Sprite.call(this, 256, 256);
 			this.x = x;
 			this.y = y;
-			switch (subject) {
-				case 0:
-					this.image = core.assets['img/island_j.png'];
-					break;
-				case 1:
-					this.image = core.assets['img/island_m.png'];
-					break;
-				case 2:
-					this.image = core.assets['img/island_sc.png'];
-					break;
-				case 3:
-					this.image = core.assets['img/island_so.png'];
-					break;
-				case 4:
-					this.image = core.assets['img/island_e.png'];
-					break;
-			}
+			this.image = core.assets[islandImage[subject]];
+			this.subject = subject;
 			core.rootScene.addChild(this);
 		},
 		ontouchstart: function() {
-			var islandMap = new IslandMap(state[subject]);
+			var islandMap = new IslandMap(state[this.subject]);
 			core.pushScene(islandMap);
         	}
 	});
@@ -53,36 +39,27 @@ window.onload = function() {
 	var IslandMap = Class.create(Scene, {
 		initialize: function(state) {
 			Scene.call(this);
-
+			console.log(state)
 			var bg = new Sprite(800,600);
 			bg.image = core.assets['img/Japanese.png'];
 			bg.x = 0;
 			bg.y = 0;
 			this.addChild(bg);
 
-			//var backButton = new BackButton();
 			var backButton = new Label('他の島へ行く');
 			backButton.addEventListener('touchstart', function() {
 				core.popScene();
             	});
 			this.addChild(backButton);
 
-			var dungeon0 = new Dungeon(80,140);
-			this.addChild(dungeon0);
-			var dungeon1 = new Dungeon(310,300);
-			this.addChild(dungeon1);
-			var dungeon2 = new Dungeon(570,225);
-			this.addChild(dungeon2);
-			var dungeon3 = new Dungeon(20,400);
-			this.addChild(dungeon3);
-			var dungeon4 = new Dungeon(480,500);
-			this.addChild(dungeon4);
+			for (var i = 0; i < dungeonOrigin.length; i++){
+ 				this.addChild(new Dungeon(dungeonOrigin[i][0], dungeonOrigin[i][1]));
+			}
 
-			var arrow = new Sprite(80,80);
-			arrow.image = core.assets['img/arrow.png'];
-			arrow.x = 125;
-			arrow.y = 60;
-			this.addChild(arrow);
+			this.addChild(new NextArrow(state));
+			for (var i = 0; i < state; i++) {
+				this.addChild(new Complete(i));
+			}
 
 			var board = new Sprite(200,100);
 			board.x = 300;
@@ -91,6 +68,24 @@ window.onload = function() {
 			this.addChild(board);
 		}
 	});
+
+	var NextArrow = Class.create(Sprite, {
+		initialize: function(state) {
+			Sprite.call(this, 80, 80);
+			this.image = core.assets['img/arrow.png'];
+			this.x = dungeonOrigin[state][0] + 90 - 40;
+			this.y = dungeonOrigin[state][1] - 80;
+		}
+	});
+
+	var Complete = Class.create(Sprite, {
+		initialize: function(state) {
+			Sprite.call(this, 180, 90);
+			this.image = core.assets['img/complete.png'];
+			this.x = dungeonOrigin[state][0];
+			this.y = dungeonOrigin[state][1];
+		}
+	})
 
 	var Dungeon = Class.create(Sprite, {
 		initialize: function(x, y) {
@@ -109,11 +104,13 @@ window.onload = function() {
 	var DungeonMap = Class.create(Scene, {
 		initialize: function(pattern) {
 			Scene.call(this);
+
 			var bg = new Sprite(800,600);
-			bg.image = core.assets['img/dungeonMap.jp'];
+			bg.image = core.assets['img/dungeonMap.jpg'];
 			bg.x = 0;
 			bg.y = 0;
 			this.addChild(bg);
+
 			var back = new Label('ダンジョンから抜け出す');
 			this.addChild(back);
 			back.on('touchstart', function() {
@@ -134,11 +131,10 @@ window.onload = function() {
 		map.y = 0;
 		core.rootScene.addChild(map);
 
-		var japanese = new Island(0,5,subject.japanese);
-		var math = new Island(544,5,subject.math);
-		var science = new Island(0,344,subject.science);
-		var social = new Island(544,344,subject.social);
-		var last = new Island(272,172,subject.english);
+		for (var i = 0; i < islandOrigin.length; i++) {
+			new Island(islandOrigin[i][0], islandOrigin[i][1], i);
+		}
+
 	};
 	core.start();
 };
