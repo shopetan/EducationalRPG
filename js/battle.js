4700f7e323416f9c4d46c6323778eeb99bb06830
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-09-15 22:19:04 +0900

    クリックイベントの確認

--- js/battle.js
+++ js/battle.js
@@ -34,46 +34,46 @@ window.onload = function () {
         return isAnswer;
     }
     
-    game.onload =
-        function () {
-            var scene = new Scene();
-            var userHp = "HP : "
-            userHp.font = "16px Tahoma";
-            var hp = 0;
-            
-            game.score = 10;
-            
-            var label = new Label(userHp);
-            scene.addChild(label)
-            game.pushScene(scene);
-            
-            var player = new Sprite(176,176);
-            player.image = game.assets['./img/dq.jpg'];
-            player.scaleX = 0.5;
-            player.scaleY = 0.5;
-            player.x = -50;
-            player.y = -30;
-            
-            var question = new Sprite(500, 100);
-            question.backgroundColor = "rgba(200, 255, 200, 0.5)";
-            question.x = 100;
-            question.y = 0;
-            
-            var surface = new Surface(500, 100);
-            question.image = surface;
-            
-            question.addEventListener('touchstart', function() {
-                question.x += 1;
-            });
-            
-            context = surface.context;
-            context.beginPath();
-            context.stroke();
-            
-            game.rootScene.addChild(player);
-            game.rootScene.addChild(question);
-        
-        };
+    game.onload = function () {
+        var scene = new Scene();
+        var backGround = new Sprite(800,600);
+        game.rootScene.addChild(backGround);        
+        game.score = 10;
+        var userHp = "HP : "
+        var hp = 5;
+        userHp.font = "16px Tahoma";
 
+        var status = new Label(userHp + hp);
+        scene.addChild(status)
+        
+        var player = new Sprite(176,176);
+        player.image = game.assets['./img/dq.jpg'];
+        player.scaleX = 0.5;
+        player.scaleY = 0.5;
+        player.x = -50;
+        player.y = -30;
+        
+        var question = new Sprite(500, 100);
+        question.backgroundColor = "rgba(200, 255, 200, 0.5)";
+        question.x = 100;
+        question.y = 0;
+
+        scene.addChild(player);
+        scene.addChild(question);
+        
+        question.addEventListener('touchstart', function() {
+            console.log("hp");
+            hp -= 1;
+            var status = new Label(userHp + hp);
+            scene.addChild(status)
+            
+            var clean = new Label(userHp + "  ");
+            scene.addChild(clean)
+                        
+        });
+        game.pushScene(scene);        
+        
+    };
+    
     game.start();
 }
