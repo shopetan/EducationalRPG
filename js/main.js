3eede609bd1b320c697ce035ba13938d4be05609
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-09-30 15:11:56 +0900

    問題提示用のクラス作成

--- js/main.js
+++ js/main.js
@@ -348,7 +348,8 @@ window.onload = function() {
         	this.addChild(status);
         	this.addChild(new Player());
         	this.addChild(new Enemy());
-        	this.addChild(new Question());
+        	this.addChild(new QuestionBase());
+            this.addChild(new Question());
         	this.addChild(new Selection(0,choiceQuestion));
         	this.addChild(new Selection(1,choiceQuestion));
         	this.addChild(new Selection(2,choiceQuestion));
@@ -368,7 +369,7 @@ window.onload = function() {
 			this.image = core.assets[PLAYER_IMG];
 		}
 	});
-	var Question = Class.create(Sprite, {
+	var QuestionBase = Class.create(Sprite, {
 		initialize: function() {
 			Sprite.call(this, 500, 100);
 			this.backgroundColor = "rgba(200, 255, 200, 0.5)";
@@ -376,6 +377,15 @@ window.onload = function() {
         		this.y = 0;
 		}
 	});
+    var Question = Class.create(Sprite, {
+		initialize: function() {
+			Sprite.call(this, 500, 100);
+			this.backgroundColor = "rgba(200, 200, 200, 0.5)";
+            this.x = 100;
+        	this.y = 0;
+		}
+	});
+    
 	var Enemy = Class.create(Sprite, {
 		initialize: function() {
 			Sprite.call(this, 800, 400);
@@ -424,16 +434,6 @@ window.onload = function() {
             }
 		}
 	});
-    
-	function attackEffect(){
-        console.log(core.currentScene);
-        addEffect(400, 300);
-    }
-    
-    function damageEffect(){
-        addEffect(400, 300);
-    }
-    
 	var GameOverScene = Class.create(Scene, {
 		initialize: function() {
 			Scene.call(this);
@@ -450,7 +450,17 @@ window.onload = function() {
 			core.pushScene(new WorldMap);
 		}
 	});
+        
     
+	function attackEffect(){
+        console.log(core.currentScene);
+        addEffect(400, 300);
+    }
+    
+    function damageEffect(){
+        addEffect(400, 300);
+    }
+        
 	function csv2Array(filePath) { //csvﾌｧｲﾙﾉ相対ﾊﾟｽor絶対ﾊﾟｽ
 		var csvData = new Array();
 	    var data = new XMLHttpRequest();
