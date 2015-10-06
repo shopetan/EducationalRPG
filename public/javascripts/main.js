2b45a9264b9156306b88f48a2b0b29a032f1a8f4
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-10-07 00:54:43 +0900

    選択修正

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -734,7 +734,6 @@ window.onload = function() {
                     }else{
                         if(count <= 1 && !(win_flag) ){
                             var isFourChoiceQuestion = records[clearProblemNum].isFourChoiceQuestion;
-                            isFourChoiceQuestion = !(isFourChoiceQuestion)
                             var problemText = records[clearProblemNum].question;
                             var problemAnswer = records[clearProblemNum].answer;
                             var problemSelect = new Array();
